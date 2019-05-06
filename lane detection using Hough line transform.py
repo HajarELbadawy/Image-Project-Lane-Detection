72e9b1595bca9e557d58c1051bd4200fb4a6ac2d
Yasminmohamed96 <myasmin91@yahoo.com>
2019-05-06 23:58:54 +0200

    Update lane detection using Hough line transform.py

--- lane detection using Hough line transform.py
+++ lane detection using Hough line transform.py
@@ -1,6 +1,6 @@
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
    image = frame.array
-   #ؤonvert  frame to gray color   
+   #convert  frame to gray color   
    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
    #we use canny edge detection to find edges 
@@ -13,11 +13,11 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
    #theta: The resolution of the parameter \theta in radians. We use 1 degree (CV_PI/180)
    #threshold: The minimum number of intersections to “detect” a line
    lines = cv2.HoughLinesP(edged,1,np.pi/180,10,minLineLength,maxLineGap)
-
+   #these for lops is to draw the lines 
    if(lines.all() !=None):
+    #lines: A vector that stores the parameters (x_{start}, y_{start}, x_{end}, y_{end}) of the detected lines from haugh
        for x in range(0, len(lines)):
            for x1,y1,x2,y2 in lines[x]:
-            #lines: A vector that stores the parameters (x_{start}, y_{start}, x_{end}, y_{end}) of the detected lines
                cv2.line(image,(x1,y1),(x2,y2),(0,255,0),2)
                #math.atan2((y2-y1),(x2-x1)) to find the slope of the line or theta 
                theta=theta+math.atan2((y2-y1),(x2-x1))
